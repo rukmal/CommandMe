c17cb0e272d295b80b8789e480cc1b0db7a70758
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-07-14 14:34:28 +0530

    Added some more tests, and got junit working correctly

--- src/me/rukmal/commandme/CommandSetter.java
+++ src/me/rukmal/commandme/CommandSetter.java
@@ -16,26 +16,30 @@ import java.lang.Exception;
 public class CommandSetter {
 	private List<Map<String, String>> args;
 
+	/**
+	 * Constructor to instantiate a new CommandSetter object
+	 */
 	public CommandSetter() {
 		args = new ArrayList<Map<String, String>>();
 	}
 
 	/**
 	 * Function to add a new option to the cli tool
-	 * @param	shortOption			Short option (eg: 'e' or 'f')
-	 * @param 	longOption			Long option (eg: 'errors' or 'file)
-	 * @param 	description 		Short description of the option (eg: Using f searches for the file)
-	 * @throws	java.lang.Exception	Thrown if the option has already been added
+	 * @param	shortOption							Short option (eg: 'e' or 'f')
+	 * @param 	longOption							Long option (eg: 'errors' or 'file)
+	 * @param 	description 						Short description of the option (eg: Using f searches for the file)
+	 * @throws	java.lang.IllegalArgumentException	Thrown if the option has already been added
 	 */
-	public void setOption(String shortOption, String longOption, String description) throws Exception {
+	public void setOption(String shortOption, String longOption, String description) throws IllegalArgumentException {
 		Map<String, String> newArg = new HashMap<String, String>();
 		newArg.put("shortOption", shortOption);
 		newArg.put("longOption", longOption);
 		newArg.put("description", description);
 		for (Map<String, String> map : args) {
 			if (map.get("shortOption").equals(shortOption)) {
-				throw new Exception("Option " + shortOption + " has already been added.");
+				throw new IllegalArgumentException("Option " + shortOption + " has already been added.");
 			}
 		}
+		args.add(newArg);
 	}
 }
--- src/tests/CommandSetterTest.java
+++ src/tests/CommandSetterTest.java
@@ -1,14 +1,47 @@
 package tests;
 
-import static org.junit.Assert;
 import me.rukmal.commandme.CommandSetter;
 import org.junit.Assert;
+import org.junit.Test;
 
-
+/**
+ * Testing class for all of the tests for the CommandSetter class
+ *
+ * @author Rukmal Weerawarana
+ */
 public class CommandSetterTest {
-	public void testSetOptionDuplicates() throws Exception {
+	private CommandSetter testcmd;
+
+	public CommandSetterTest() {
+		testcmd = new CommandSetter();
+	}
+
+	/**
+	 * Test to check whether a command can be added using the command setter
+	 */
+	@Test
+	public void testAddingCommand() {
+		try {
+			testcmd.setOption("f", "find", "Find a file");
+		} catch (Exception e) {
+			Assert.fail();
+		}
+	}
+
+	/**
+	 * Test to ensure that the correct error is being thrown when a duplicate argument is added
+	 */
+	@Test
+	public void testDuplicateCommandAddition() {
 		CommandSetter cmd = new CommandSetter();
 		cmd.setOption("f", "find", "Find a file");
-		Assert.
+		try {
+			cmd.setOption("f", "find", "Find a file");
+			Assert.fail();
+		} catch (IllegalArgumentException e) {
+			if (!e.getMessage().equals("Option f has already been added.")) {
+				Assert.fail();
+			}
+		}
 	}
 }
